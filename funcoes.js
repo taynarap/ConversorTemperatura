b610de10cc733d11a43464e6c000beba9646376d
Taynara <77408568+taynarap@users.noreply.github.com>
2023-05-15 20:31:37 +0100

    Add files via upload

---
+++ funcoes.js
@@ -0,0 +1,54 @@
+// ---------- BTN CALCULADORA ---------- 
+
+function digitar(btn){
+    let ecra = document.getElementById("valor");
+
+    if(ecra.innerHTML.length < 10){
+        if (btn == "."){
+            if(!ecra.innerHTML.includes(".")){
+                ecra.innerHTML += btn;
+            }
+        } else {
+            ecra.innerHTML += btn;
+        }
+    }
+
+    resultado(parseFloat(ecra.innerHTML));
+}
+
+// ---------- CALCULOS PARA K E F ---------- 
+
+function resultado(valor){
+    let resultado = document.getElementById("resultado");
+    let celsius = parseFloat(valor);
+    
+    
+    if(!isNaN(celsius)){
+        let k = celsius + 273;
+        let f = (celsius * 1.8)+32;
+        resultado.innerHTML= celsius+ " ºC = " + f.toFixed(2) + " ºF <br><br>";
+        resultado.innerHTML+= celsius + " ºC = " + k.toFixed(2) + " ºK <br>";
+    }
+    else{
+        resultado.innerHTML="";
+    }
+}
+
+// ---------- BTN AC ---------- 
+
+function limparTudo(){
+    let ecra = document.getElementById("valor");
+    let resultado = document.getElementById("resultado");
+
+    ecra.innerHTML="";
+    resultado.innerHTML="";
+}
+
+// ---------- BTN DE ---------- 
+
+function limparUltimo(){
+    let ecra = document.getElementById("valor");
+
+    ecra.innerHTML = ecra.innerHTML.toString().slice(0,-1);
+    resultado(parseFloat(ecra.innerHTML));
+}
